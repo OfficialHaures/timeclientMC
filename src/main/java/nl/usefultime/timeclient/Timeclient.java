5adb4cc24dbea5adc99ebafd49d3b080ce35871b
OfficialHaures <166564037+OfficialHaures@users.noreply.github.com>
2025-02-27 20:56:01 +0100

    ...

--- src/main/java/nl/usefultime/timeclient/Timeclient.java
+++ src/main/java/nl/usefultime/timeclient/Timeclient.java
@@ -1,91 +1,69 @@
 package nl.usefultime.timeclient;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import nl.usefultime.timeclient.features.AutoGG;
+import nl.usefultime.timeclient.features.visual.*;
+import nl.usefultime.timeclient.features.performance.*;
+import nl.usefultime.timeclient.features.social.*;
+import nl.usefultime.timeclient.features.hud.*;
+import nl.usefultime.timeclient.gui.TimeClientGui;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(Timeclient.MODID)
+@Mod("timeclient")
 public class Timeclient {
+    private static Timeclient instance;
+    private String name = "TimeClient";
+    private String version = "1.0";
 
-    // Define mod id in a common place for everything to reference
-    public static final String MODID = "timeclient";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "timeclient" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "timeclient" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Creates a new Block with the id "timeclient:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "timeclient:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+    // Feature instances
+    private CustomCrosshair customCrosshair = new CustomCrosshair();
+    private BlockOverlay blockOverlay = new BlockOverlay();
+    private RAMDisplay ramDisplay = new RAMDisplay();
+    private FPSDisplay fpsDisplay = new FPSDisplay();
+    private PingDisplay pingDisplay = new PingDisplay();
+    private PotionEffectsHUD potionEffectsHUD = new PotionEffectsHUD();
+    private ParticleMultiplier particleMultiplier = new ParticleMultiplier();
 
     public Timeclient() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
+        instance = this;
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+        init();
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    public void init() {
+        // Register all features to Forge Event Bus
+        registerFeatures();
     }
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
+    private void registerFeatures() {
+        MinecraftForge.EVENT_BUS.register(customCrosshair);
+        MinecraftForge.EVENT_BUS.register(blockOverlay);
+        MinecraftForge.EVENT_BUS.register(ramDisplay);
+        MinecraftForge.EVENT_BUS.register(fpsDisplay);
+        MinecraftForge.EVENT_BUS.register(pingDisplay);
+        MinecraftForge.EVENT_BUS.register(potionEffectsHUD);
+        MinecraftForge.EVENT_BUS.register(particleMultiplier);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+    public void openGui() {
+        Minecraft.getInstance().setScreen(new TimeClientGui().getScreen());
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    // Getters
+    public CustomCrosshair getCustomCrosshair() { return customCrosshair; }
+    public BlockOverlay getBlockOverlay() { return blockOverlay; }
+    public RAMDisplay getRamDisplay() { return ramDisplay; }
+    public FPSDisplay getFpsDisplay() { return fpsDisplay; }
+    public PingDisplay getPingDisplay() { return pingDisplay; }
+    public PotionEffectsHUD getPotionEffectsHUD() { return potionEffectsHUD; }
+    public ParticleMultiplier getParticleMultiplier() { return particleMultiplier; }
 
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
+    public static Timeclient getInstance() {
+        return instance;
     }
+    public String getVersion() {
+        return version;
+    }
+
 }
